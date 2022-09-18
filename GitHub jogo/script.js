16c9bf4bd071a792d5bdca01a14494c7cf94f720
INDOMINUS13579 <72354782+INDOMINUS13579@users.noreply.github.com>
2022-09-18 16:21:26 -0300

    Adicionei o jogo do super trunfo

---
+++ GitHub jogo/script.js
@@ -0,0 +1,337 @@
+var carta1 = {
+  nome: "Oziel Keyno",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/306931394_1317128202027308_6171053247234467434_n.jpg?stp=dst-jpg_e15_p320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=CO-YrFPq2sAAX-gIejz&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTA1NTQ4Njg5MzM5MDQ1Mw%3D%3D.2-ccb7-5&oh=00_AT8fMoHnJnCjQJ2e_aaZVxW0esnBJnFLrN3a55bZiuArfQ&oe=632D26BF&_nc_sid=fa978c",
+  atributos: {
+    ataque: 2,
+    defesa: 9,
+    magia: 6
+  }
+};
+
+var carta2 = {
+  nome: "DxMg Arthur",
+  imagem:
+    "https://scontent.cdninstagram.com/v/t1.15752-9/306064897_803593490953991_3271336011237170083_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5a057b&_nc_ohc=Hphksza7ueQAX8pyBit&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AVJWMVL-USUzM0Heq1SNHnVvXCglrBBKJiFzm3wiba8_zQ&oe=6349E179",
+  atributos: {
+    ataque: 8,
+    defesa: 3,
+    magia: 6
+  }
+};
+
+var carta3 = {
+  nome: "LittleJohn",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/306865980_812562189762789_4841765337596951862_n.jpg?stp=dst-jpg_e15_p320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=6FKEuD-gyMQAX9dmvSV&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTA1NjM4NjA5MzQ2MTQxOA%3D%3D.2-ccb7-5&oh=00_AT8vMwPpw0TedT-89RPYTA18dAwHN8VsvBL0DMYfE6V-Zw&oe=632CF266&_nc_sid=fa978c",
+  atributos: {
+    ataque: 5,
+    defesa: 10,
+    magia: 2
+  }
+};
+
+var carta4 = {
+  nome: "Oniverso",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307529504_5828945850469129_1523165961000138373_n.jpg?stp=dst-jpg_e15_p320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=W00B0i5Z7bgAX9w42-F&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTA1NDY4MDUzMDE1MzYzOA%3D%3D.2-ccb7-5&oh=00_AT8oLLbsbiRzjsGWCzJcDPdB--OQ0nsR3vFNUr_jBzrYbQ&oe=632C202F&_nc_sid=fa978c",
+  atributos: {
+    ataque: 5,
+    defesa: 2,
+    magia: 10
+  }
+};
+
+var carta5 = {
+  nome: "Xbeifong",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307254217_751620502798490_891661554438895219_n.jpg?stp=dst-jpg_e15_p320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=X3bg4IvnLH8AX872c0Z&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTA1MzI0ODQwMTg0NTE4MQ%3D%3D.2-ccb7-5&oh=00_AT-91vFSAMgwmEmwXZr4WuzZEG0R0PhJSqLKIjw7iv0T4g&oe=632C7D26&_nc_sid=fa978c",
+  atributos: {
+    ataque: 7,
+    defesa: 1,
+    magia: 3
+  }
+};
+
+var carta6 = {
+  nome: "Ghost",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307098217_586832459844960_5272485429612901480_n.jpg?stp=dst-jpg_e15_p320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=UM-L0GAyOQYAX9-DiTm&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTA2NDcxNzYzMzk1NDY0OA%3D%3D.2-ccb7-5&oh=00_AT-8oQNRcjtVuptXFmVxzefLBz-LR_9bqwyKjeIOo_AQPg&oe=632C3026&_nc_sid=fa978c",
+  atributos: {
+    ataque: 2,
+    defesa: 6,
+    magia: 5
+  }
+};
+
+var carta7 = {
+  nome: "Fabricio SDW",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307023870_502253001312733_2589241873587780549_n.jpg?stp=dst-jpg_e15_s320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=GOmbzWq_JBwAX-x8b4U&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTUwNTEzMjQzOTU1MzE2Mg%3D%3D.2-ccb7-5&oh=00_AT-F790wiLSrcEEXV5ZRkXBIszqMHaUgQbz4ajbkFQkisg&oe=632D35AA&_nc_sid=fa978c",
+  atributos: {
+    ataque: 3,
+    defesa: 6,
+    magia: 7
+  }
+};
+
+var carta8 = {
+  nome: "BrunoCarbhiaki",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307115872_182639890922561_230031350186561490_n.jpg?stp=dst-jpg_e15_p320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=vnj1vbb2LzQAX9Dn0gE&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTY2NDcyNDA5NTUwNTU0NQ%3D%3D.2-ccb7-5&oh=00_AT_FBAWc1hZzD18HlvomyIyMp_r3-aFf-UwrjvdOEz-ITw&oe=632DDACF&_nc_sid=fa978c",
+  atributos: {
+    ataque: 3,
+    defesa: 2,
+    magia: 10
+  }
+};
+
+var carta9 = {
+  nome: "IndomiNerd (GOLD)",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307561056_189340586906692_6136022350651015196_n.jpg?stp=dst-jpg_e15_s320x320&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=flKo2ibHVysAX8qY3Yt&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTYxMjEwNzQ3NDA0ODg2NQ%3D%3D.2-ccb7-5&oh=00_AT9agqQBflsXtPtmS7OT8p8jBs8fv4gqShqz7MghvCUYzQ&oe=632DD922&_nc_sid=fa978c",
+  atributos: {
+    ataque: 10,
+    defesa: 10,
+    magia: 10
+  }
+};
+
+var carta10 = {
+  nome: "R4nieri",
+  imagem:
+    "https://instagram.fbnu1-1.fna.fbcdn.net/v/t51.2885-15/307438762_613270067167301_7804915938857628779_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fbnu1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=oGg8YyUrpGcAX-zBR4k&edm=ABJHkxYAAAAA&ccb=7-5&ig_cache_key=MjkyOTY1MjQxNDA5ODU5MDA3Ng%3D%3D.2-ccb7-5&oh=00_AT-63hvOuCRSZFEg3fum2whhZp6DCFO2wDa-YMdSc1STeA&oe=632D1FA9&_nc_sid=fa978c",
+  atributos: {
+    ataque: 5,
+    defesa: 6,
+    magia: 2
+  }
+};
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10
+];
+
+var cartaMaquina;
+var cartaJogador;
+
+var cartasDoJogador = [];
+var cartasDaMaquina = [];
+
+function sortearCarta() {
+  var botaoSortear = document.getElementById("btnSortear");
+  var botaoJogar = document.getElementById("btnJogar");
+
+  var numeroCartaMaquina = parseInt(Math.random() * 10);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 10);
+
+  while (numeroCartaMaquina === numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * 10);
+  }
+
+  cartaJogador = cartas[numeroCartaJogador];
+
+  botaoSortear.disabled = true;
+  botaoJogar.disabled = false;
+
+  exibeCardJogador();
+}
+
+function exibeCardJogador() {
+  var card = document.getElementById("opcoes");
+  var cardTexto = "";
+
+  cardTexto += `
+    <div class="cartao">
+      <img class="cartao__imagem" src="${cartaJogador.imagem}" alt="">
+      <div class="cartao__info" id="cartao-info">
+        <p>${cartaJogador.nome}</p>
+
+      </div>
+    </div>
+  `;
+
+  card.innerHTML = cardTexto;
+
+  exibeOpcoes();
+}
+
+function exibeOpcoes() {
+  var opcoes = document.getElementById("cartao-info");
+  var opcoesTexto = `<p class="info__titulo">${cartaJogador.nome}</p>`;
+
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto += `
+      <div class="info__atributos">
+        <label class="atributo">
+          <input type="radio" name="atributo" value="${atributo}" checked> 
+          ${atributo}
+        </label>
+        <p class="atributo__valor">${cartaJogador.atributos[atributo]}</p>
+      </div>
+    `;
+  }
+
+  opcoes.innerHTML = opcoesTexto;
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked === true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var elementoResultado = document.getElementById("resultado");
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+  var botaoJogarNovamente = document.getElementById("btnJogarNovamente");
+  var botaoJogar = document.getElementById("btnJogar");
+
+  if (valorCartaJogador > valorCartaMaquina) {
+    elementoResultado.innerHTML = `<p>O atributo selecionado foi ${atributoSelecionado}. Você venceu. Parabéns! Vamos para a próxima rodada.`;
+    cartasDoJogador.push(cartaMaquina);
+  } else if (valorCartaJogador === valorCartaMaquina) {
+    elementoResultado.innerHTML = `<p>O atributo selecionado foi ${atributoSelecionado}. Você empatou. Vamos para a próxima rodada.`;
+  } else {
+    elementoResultado.innerHTML = `<p>O atributo selecionado foi ${atributoSelecionado}. Você perdeu. Que pena! Vamos para a próxima rodada.`;
+    cartasDaMaquina.push(cartaJogador);
+  }
+
+  if (cartasDaMaquina.length == 5 || cartasDoJogador.length == 5) {
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnJogarNovamente").disabled = true;
+    document.getElementById("btnSortear").disabled = true;
+
+    if (cartasDaMaquina.length == 5) {
+      elementoResultado.innerHTML = `<p>Fim do jogo. O vencedor foi a máquina por ${cartasDaMaquina.length} a ${cartasDoJogador.length} pontos.`;
+      document.getElementById("divBotaoReiniciar").innerHTML = `
+      <button type="button" onclick=reiniciar()>Reiniciar</button>
+    `;
+    } else {
+      elementoResultado.innerHTML = `<p>Fim do jogo. O vencedor foi você por ${cartasDoJogador.length} a ${cartasDaMaquina.length}.`;
+      document.getElementById("divBotaoReiniciar").innerHTML = `
+      <button type="button" onclick=reiniciar()>Reiniciar</button>
+    `;
+    }
+  } else {
+    document.getElementById("btnJogarNovamente").disabled = false;
+  }
+
+  botaoJogar.disabled = true;
+
+  exibeCards();
+  exibePontuacao();
+}
+
+function exibeCards() {
+  var card = document.getElementById("opcoes");
+  var cardTexto = "";
+  cardTexto += `<div class="cartao">
+      <img class="cartao__imagem" src="${cartaJogador.imagem}" alt="">
+      <div class="cartao__info cartao__info--novo" id="cartao-info">
+        <p class="info__titulo">${cartaJogador.nome}</p>
+        <div>
+          <p>ATAQUE</p>
+          <p>${cartaJogador.atributos["ataque"]}</p>
+        </div>
+        <div>
+          <p>DEFESA</p>
+          ${cartaJogador.atributos["defesa"]}
+        </div>
+        <div>
+          <p>MAGIA</p>
+          ${cartaJogador.atributos["magia"]}
+        </div>
+      </div>
+    </div>
+  `;
+
+  cardTexto += `
+    <div class="cartao">
+      <img class="cartao__imagem" src="${cartaMaquina.imagem}" alt="">
+      <div class="cartao__info cartao__info--novo" id="cartao-info">
+        <p class="info__titulo">${cartaMaquina.nome}</p>
+        <div>
+          <p>ATAQUE</p>
+          <p>${cartaMaquina.atributos["ataque"]}</p>
+        </div>
+        <div>
+          <p>DEFESA</p>
+          <p>${cartaMaquina.atributos["defesa"]}</p>
+        </div>
+        <div>
+          <p>MAGIA</p>
+          <p>${cartaMaquina.atributos["magia"]}</p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  card.innerHTML = cardTexto;
+}
+
+function jogarNovamente() {
+  var botaoJogar = document.getElementById("btnJogar");
+  var botaoSortear = document.getElementById("btnSortear");
+  var botaoJogarNovamente = document.getElementById("btnJogarNovamente");
+  var elementoResultado = document.getElementById("resultado");
+  var card = document.getElementById("opcoes");
+
+  cartaJogador = "";
+  cartaMaquina = "";
+  elementoResultado.innerHTML = "";
+  card.innerHTML = "";
+
+  botaoJogar.disabled = true;
+  botaoSortear.disabled = false;
+  botaoJogarNovamente.disabled = true;
+
+  sortearCarta();
+}
+
+function exibePontuacao() {
+  var pontuacao = document.getElementById("pontuacao");
+
+  pontuacao.innerHTML = `<span>Jogador: ${cartasDoJogador.length}</span> <span>Máquina: ${cartasDaMaquina.length}</span>`;
+}
+
+function reiniciar() {
+  var botaoJogar = document.getElementById("btnJogar");
+  var botaoSortear = document.getElementById("btnSortear");
+  var botaoJogarNovamente = document.getElementById("btnJogarNovamente");
+  var elementoResultado = document.getElementById("resultado");
+  var card = document.getElementById("opcoes");
+  var pontuacao = document.getElementById("pontuacao");
+  var divBotao = document.getElementById("divBotaoReiniciar");
+
+  cartaJogador = "";
+  cartaMaquina = "";
+  elementoResultado.innerHTML = "";
+  card.innerHTML = "";
+  pontuacao.innerHTML = "";
+  divBotao.innerHTML = "";
+
+  cartasDaMaquina = [];
+  cartasDoJogador = [];
+
+  botaoJogar.disabled = true;
+  botaoSortear.disabled = false;
+  botaoJogarNovamente.disabled = true;
+}
